ed4dc206953555dda4f8a292d67c563198c0d4bb
deepak-delta <ddk3857@gmail.com>
2022-04-12 17:57:30 +0530

    Model and Database setup

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,10 +1,12 @@
 const asyncHander = require('express-async-handler')
+const Todo = require('../models/todoModel')
 
 // @desc Get ToDO List
 // @route GET /api/todo
 // @access Private
 const getTodo = asyncHander(async (req, res) => {
-  res.status(200).json({ message: 'GET' })
+  const todos = await Todo.find()
+  res.status(200).json(todos)
 })
 
 // @desc Get ToDO List
@@ -15,21 +17,39 @@ const setTodo = asyncHander(async (req, res) => {
     res.status(400)
     throw new Error('Bad Request')
   }
-  res.status(200).json({ message: 'POST' })
+
+  const todo = await Todo.create(req.body)
+  res.status(200).json(todo)
 })
 
 // @desc Get ToDO List
 // @route PUT /api/todo/:id
 // @access Private
 const updateTodo = asyncHander(async (req, res) => {
-  res.status(200).json({ message: `Update ${req.params.id}` })
+  const todos = await Todo.findById(req.params.id)
+  if (!todos) {
+    res.status(404)
+    throw new Error('Not Found')
+  }
+
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+  res.status(200).json(updatedTodo)
 })
 
 // @desc Get ToDO List
 // @route DELETE /api/todo/:id
 // @access Private
 const deleteTodo = asyncHander(async (req, res) => {
-  res.status(200).json({ message: `Delete ${req.params.id}` })
+  const todos = await Todo.findById(req.params.id)
+  if (!todos) {
+    res.status(404)
+    throw new Error('Not Found')
+  }
+
+  await Todo.remove()
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
